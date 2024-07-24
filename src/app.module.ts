aad2d36fe94d7880a30b287bd2379ff8e559ae64
Davi Taveira <devtaveira@gmail.com>
2024-07-24 15:18:11 -0300

    updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WhatsAppModule } from './whatsapp/whatsapp.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WhatsAppModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    WhatsAppModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -8,11 +8,44 @@ export class WhatsAppService implements OnModuleInit {
 
   async onModuleInit() {
 
-    this.client = await create('session-name', undefined, (base64Qrimg, asciiQR, attempts) => {
-      console.log('Number of attempts to read the qrcode: ', attempts);
-      console.log('Terminal qrcode: ', asciiQR);
-      this.qrCode = base64Qrimg;
-    });
+    this.client = await create(
+      //session
+      'session', //Pass the name of the client you want to start the bot
+      //catchQR
+      (base64Qrimg, asciiQR, attempts, urlCode) => {
+        console.log('Number of attempts to read the qrcode: ', attempts);
+        console.log('Terminal qrcode: ', asciiQR);
+        console.log('base64 image string qrcode: ', base64Qrimg);
+        console.log('urlCode (data-ref): ', urlCode);
+      },
+      // statusFind
+      (statusSession, session) => {
+        console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
+        //Create session wss return "serverClose" case server for close
+        console.log('Session name: ', session);
+      },
+      // options
+      {
+        browserPathExecutable: '', // browser executable path
+        folderNameToken: 'tokens', //folder name when saving tokens
+        mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+        headless: 'new', // you should no longer use boolean false or true, now use false, true or 'new' learn more https://developer.chrome.com/articles/new-headless/
+        devtools: false, // Open devtools by default
+        debug: false, // Opens a debug session
+        logQR: true, // Logs QR automatically in terminal
+        browserWS: '', // If u want to use browserWSEndpoint
+        browserArgs: [''], // Original parameters  ---Parameters to be added into the chrome browser instance
+        addBrowserArgs: [''], // Add broserArgs without overwriting the project's original
+        puppeteerOptions: {}, // Will be passed to puppeteer.launch
+        disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+        disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+        updatesLog: true, // Logs info updates automatically in terminal
+        autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+        createPathFileToken: false, // creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
+        addProxy: [''], // Add proxy server exemple : [e1.p.webshare.io:01, e1.p.webshare.io:01]
+        userProxy: '', // Proxy login username
+        userPass: '' // Proxy password
+      })
     
 
     console.log(`
